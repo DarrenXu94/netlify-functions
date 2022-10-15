15c0e91b7fae6e639ed6c78ad69e374a79268de7
Darren Xu <darrenxu1994@gmail.com>
2022-10-16 10:00:15 +1100

    Football API

---
+++ README.md
@@ -0,0 +1,9 @@
+# Trying Netlify functions
+
+Routes found at this url
+- /.netlify/functions/hello
+
+Run with 
+```
+netlify dev
+```
---
+++ netlify/functions/football.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+require("dotenv").config();
+
+async function callAPI() {
+  return axios.get("http://api.football-data.org/v4/competitions/PL", {
+    headers: {
+      "X-Auth-Token": process.env.NETLIFY_API_KEY,
+    },
+  });
+}
+exports.handler = async function (event, context) {
+  const res = await callAPI();
+  console.log(res);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(res.data),
+  };
+};
