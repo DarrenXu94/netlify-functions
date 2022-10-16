8ff96c638bd1333fa9110a23f31412ae9d49a167
Darren Xu <darrenxu1994@gmail.com>
2022-10-16 17:11:39 +1100

    Remove showing process env

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 
 exports.handler = async function (event, context) {
-  console.log(process.env);
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: process.env }),
+    body: JSON.stringify({ message: "hello world" }),
   };
 };
